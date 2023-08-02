bfc641730a9e6d54f9e8eae7739e49b0451d97ab
shubham-guru <gurushubham4@gmail.com>
2023-08-03 00:58:59 +0530

    Speakers Added

--- src/constants/SpeakersList.ts
+++ src/constants/SpeakersList.ts
@@ -2,6 +2,9 @@ import speakerOne from "../assets/speaker-1.png";
 import speakerTwo from '../assets/speaker-3 1.png';
 import speakerThree from '../assets/Group 1000006689.png';
 import speakerFour from '../assets/speaker-4.png';
+import speakerFive from '../assets/speaker-5.png';
+import speakerSix from '../assets/speaker-6.png';
+
 
 export const SpeakerList = [
   {
@@ -27,5 +30,16 @@ export const SpeakerList = [
     name: "Bhupendra Kumar",
     designation: "Head of Logistics",
     company: "IOL Chemicals and Pharmaceuticals Limited",
+  },
+  {
+    image: speakerFive,
+    name: "Dr Sanajy Gupta",
+    designation: "Chief Procurement Office",
+    company: "Adani Cement",
+  },{
+    image: speakerSix,
+    name: "R Sampath Raghavan",
+    designation: "Head - Exim and Logistics",
+    company: "Raymond Group",
   },
 ];
